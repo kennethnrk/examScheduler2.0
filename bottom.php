a98ebf601f5c65175af1267019b8f1f710b5165e
Kennethnrk <kenneth012004@outlook.com>
2021-08-27 22:51:44 +0530

    changed the look of the footer

--- bottom.php
+++ bottom.php
@@ -1,10 +1,12 @@
 
-    <div class="container cont">
-   <div class="row">
-      <div class="col-xs-12 foot">
-         Created by Kenneth Rodrigues, Khush Trivedi 
+      <div class="foot">
+         <div class="container">
+            <div class="row">
+               <div class="col-xs-12 ">
+                  Created by Kenneth Rodrigues, Khush Trivedi 
+               </div>
+            </div>
+         </div>
       </div>
-   </div>
-   </div>
    </body>
 </html>
